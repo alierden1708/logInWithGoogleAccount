7441c09665ad0a326b103f274c2265d1e179d647
ali erden <alierden1708@gmial.com>
2023-02-19 10:02:23 +0300

    .views added

--- users/views.py
+++ users/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login
+# karşılama sayfası ve login olduktan sonra yönlendirmek istediğim view fonksiyonumu yazdım
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'users/home.html')
+
+
+def user_login(request):
+    # django auth formstan import ediyoruz
+    form = AuthenticationForm(request, data=request.POST or None)
+    if form.is_valid():
+        user = form.get_user()  # formdan gelen bilgileri alıyoruz
+        login(request, user)  # formda gelen bilgilerle istek atıyoruz
+        # urls.py da tanımladığımız name patterninden gelen değer
+        return redirect('home')
+    return render(request, 'users/login.html', {'form': form})
+# register için de kendiniz fonksiyon yazabilirsiniz. Dediğim gibi bu yazımda asıl odaklanacağımız kısım google ile giriş konusu o nedenle ben burada örnek bir yapı kuruyorum.
